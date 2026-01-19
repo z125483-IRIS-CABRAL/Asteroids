7f9d23cd20cf69382297b13c82ec574eab7e23ec
Z125483-IRIS-CABRAL <z125483@shibaura-it.ac.jp>
2026-01-19 19:49:03 +0900

    add asteroids_db.h

---
+++ asteroid_db.h
@@ -0,0 +1,27 @@
+// asteroid_db.h
+#ifndef ASTEROID_DB_H
+#define ASTEROID_DB_H
+
+#include <stddef.h>
+
+#define STR_MAX 128
+
+typedef struct {
+    char date[16];
+    char name[STR_MAX];
+    long id;
+    int isHazardous;
+    double absolute_magnitude_h;
+    double diameter_min_m;
+    double diameter_max_m;
+    double miss_distance_km;
+    double velocity_km_s;
+} Asteroid;
+
+typedef struct {
+    Asteroid *data;
+    size_t size;
+    size_t cap;
+} AsteroidDB;
+
+#endif
